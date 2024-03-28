0a38053ac2c9e53477ee88f2b214b5392eaacce0
Peter Gagliardi <ptrgags@gmail.com>
2024-03-28 14:54:10 -0400

    Start fleshing out index page

--- src/components/PageLink.vue
+++ src/components/PageLink.vue
@@ -1,14 +1,34 @@
 <template>
-    <div class="link">
-        <slot></slot>
+  <div class="link">
+    <slot name="thumbnail"></slot>
+    <slot name="link"></slot> &mdash;
+    <div class="description">
+      <slot name="description"></slot>
     </div>
+  </div>
 </template>
 
 <style scoped>
-    .link {
-        background-color: var(--color-background);
-        padding: 10px;
-        margin: 10px 0;
-        width: 80%;
-      }
+.link {
+  background-color: var(--color-background);
+  padding: 10px;
+  margin: 10px 0;
+  width: 50%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  gap: 10px;
+}
+
+.description {
+  max-width: 50%;
+}
+
+@media screen and (max-width: 500px) {
+  .link {
+    /** Match the size of the image */
+    width: 250px;
+  }
+}
 </style>
--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -6,26 +6,114 @@ import PageLink from '@/components/PageLink.vue'
   <div class="one-column vertical">
     <h1>Symmetry Sketchbook</h1>
     <p>
-      This repo has a few experiments based on the book <i>Creating Symmetry</i> by Frank A. Farris
-      and implemented in p5.js
+      This repo has a few artistic math experiments based on the book
+      <cite>
+        <a href="https://www.google.com/books/edition/Creating_Symmetry/blGUBgAAQBAJ?hl=en&gbpv=0"
+          >Creating Symmetry</a
+        >
+      </cite>
+      by Frank A. Farris and implemented in p5.js
     </p>
     <h3>Parametric Curve Symmetry</h3>
-    <PageLink><RouterLink to="/curve_symmetry">Curve Viewer</RouterLink></PageLink>
-    <PageLink><RouterLink to="/curve_symmetry/orbital_motion">Orbital Motion</RouterLink></PageLink>
-    <PageLink><RouterLink to="/curve_symmetry/curve_maker">Curve Maker</RouterLink></PageLink>
-    <h3>Point Symmetry</h3>
-    <PageLink><RouterLink to="/point_symmetry">Rosette Gallery</RouterLink></PageLink>
+    <p class="expand">Symmetric paths based on chapters 1-5 of the book</p>
     <PageLink>
-      <RouterLink to="/point_symmetry/tie_dye_analogy">Tie-Dye Analogy</RouterLink>
+      <template #thumbnail>
+        <img src="@/assets/thumbnails/curve-gallery.png" alt="symmetric curve example" />
+      </template>
+      <template #link>
+        <RouterLink to="/curve_symmetry">Curve Symmetry Gallery</RouterLink>
+      </template>
+      <template #description>A showcase of symmetric curves I've made.</template>
     </PageLink>
-    <PageLink><RouterLink to="/point_symmetry/rosette_maker">Rosette Maker</RouterLink></PageLink>
-    <h3>Frieze Symmetry</h3>
-    <PageLink><RouterLink to="/frieze_symmetry">Frieze Gallery</RouterLink></PageLink>
-    <PageLink><RouterLink to="/frieze_symmetry/frieze_maker">Frieze Maker</RouterLink></PageLink>
-    <h3>Wallpaper Symmetry</h3>
-    <PageLink><RouterLink to="/wallpaper_symmetry">Wallpaper Gallery</RouterLink></PageLink>
     <PageLink>
-      <RouterLink to="/wallpaper_symmetry/wallpaper_maker">Wallpaper Maker</RouterLink>
+      <template #link>
+        <RouterLink to="/curve_symmetry/curve_maker">Curve Maker</RouterLink>
+      </template>
+    </PageLink>
+    <PageLink>
+      <template #thumbnail>
+        <img src="@/assets/thumbnails/orbital-motion.png" alt="preview of orbital motion" />
+      </template>
+      <template #link>
+        <RouterLink to="/curve_symmetry/orbital_motion">Orbital Motion</RouterLink>
+      </template>
+      <template #description>
+        As a fun bonus, I realized that the code for these curves can be re-used to make a
+        (simplified) model of a solar system
+      </template>
+    </PageLink>
+
+    <h3>Point Symmetry</h3>
+    <p class="expand">
+      Patterns that are symmetric about a center point. Based on chapters 6-7 of the book.
+    </p>
+    <PageLink>
+      <template #thumbnail>
+        <img src="@/assets/thumbnails/rosette-gallery.png" alt="Rosette pattern example" />
+      </template>
+      <template #link><RouterLink to="/point_symmetry">Rosette Gallery</RouterLink></template>
+    </PageLink>
+    <PageLink>
+      <template #thumbnail>
+        <img
+          src="@/assets/thumbnails/tie-dye-analogy.png"
+          alt="screenshot from tie-dye analogy animation"
+        />
+      </template>
+      <template #link>
+        <RouterLink to="/point_symmetry/tie_dye_analogy">Tie-Dye Analogy</RouterLink>
+      </template>
+      <template #description>
+        It can be a bit tricky to understand how these patterns are produced with math. I made an
+        animation to explain it by analogy with tie-dying a t-shirt.
+      </template>
+    </PageLink>
+    <PageLink>
+      <template #link>
+        <RouterLink to="/point_symmetry/rosette_maker">Rosette Maker</RouterLink>
+      </template>
+    </PageLink>
+
+    <h3>Frieze Symmetry</h3>
+    <p class="expand">Patterns that repeat in one direction. Based on chapter 8 of the book.</p>
+    <PageLink>
+      <template #thumbnail>
+        <img src="@/assets/thumbnails/frieze-gallery.png" alt="Frieze pattern example" />
+      </template>
+      <template #link>
+        <RouterLink to="/frieze_symmetry">Frieze Gallery</RouterLink>
+      </template>
+    </PageLink>
+    <PageLink>
+      <template #link>
+        <RouterLink to="/frieze_symmetry/frieze_maker">Frieze Maker</RouterLink>
+      </template>
+    </PageLink>
+
+    <h3>Wallpaper Symmetry</h3>
+    <p class="expand">
+      Patterns that repeat in one direction. Based on chapters 9-19 as well as appendices B and C.
+    </p>
+    <PageLink>
+      <template #link>
+        <RouterLink to="/wallpaper_symmetry">Wallpaper Gallery</RouterLink>
+      </template>
+    </PageLink>
+    <PageLink>
+      <template #link>
+        <RouterLink to="/wallpaper_symmetry/wallpaper_maker">Wallpaper Maker</RouterLink>
+      </template>
     </PageLink>
   </div>
 </template>
+
+<style scoped>
+img {
+  width: 250px;
+  height: 350px;
+}
+
+.expand {
+  width: 50%;
+}
+</style>
